c0de61e8d7a445e753fb2fa209da38fb77e5ac41
Matija Kočevar <matija.kocev@gmail.com>
2025-03-03 18:33:47 +0100

    Add padding to pages

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,10 +1,10 @@
 export default function Education() {
     return (
-        <>
+        <div className='flex flex-col gap-4 p-16'>
             <h1 className='mb-6 text-3xl font-bold'>Education</h1>
             <div>
                 <p>This will be a page about my education.</p>
             </div>
-        </>
+        </div>
     );
 }
