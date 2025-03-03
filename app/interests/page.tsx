defc15e6e33672030af0965638d44f12be5d4bfc
Matija Kočevar <matija.kocev@gmail.com>
2025-03-03 18:57:24 +0100

    Center interests

--- app/interests/page.tsx
+++ app/interests/page.tsx
@@ -5,10 +5,10 @@ import ShortsGallery from './components/ShortsGallery';
 
 export default function InterestsPage() {
     return (
-        <div className='container mx-auto px-4 py-8'>
+        <div className='container mx-auto flex h-full flex-col items-center justify-center px-4 py-8'>
             <h1 className='mb-8 text-3xl font-bold'>My YouTube Content</h1>
 
-            <div className='grid grid-cols-1 gap-8 md:grid-cols-2'>
+            <div className='grid w-full grid-cols-1 gap-8 md:grid-cols-2'>
                 <div className='w-full'>
                     <VideoGallery />
                 </div>
