a4aa924ba2b997a9d7d4b38f25db1a9470471e50
Matija Kočevar <matija.kocev@gmail.com>
2025-03-25 01:13:14 +0100

    Prevent projects layout shift

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Projects from './_components/Projects';
 export default function Home() {
     return (
         <div
-            className={`flex min-h-full w-full flex-col items-center gap-8 min-[1024px]:h-full min-[1024px]:flex-row min-[1024px]:py-0`}
+            className={`flex min-h-full w-full flex-col items-center justify-between min-[1024px]:h-full min-[1024px]:flex-row min-[1024px]:py-0`}
         >
             <Bio />
             <Projects />
