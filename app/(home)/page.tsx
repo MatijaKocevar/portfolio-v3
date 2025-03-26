4e9b94c2d2daaca30c63a97454997e944067e6f1
Matija Kočevar <matija.kocev@gmail.com>
2025-03-26 03:25:09 +0100

    Fix Active Projects layout and move logic to store

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,17 @@
 import Bio from './_components/Bio';
-import Projects from './_components/Projects';
+import ActiveProjects from './_components/ActiveProjects';
 
 export default function Home() {
     return (
         <div
-            className={`flex min-h-full w-full flex-col items-center justify-between min-[1024px]:h-full min-[1024px]:flex-row min-[1024px]:py-0`}
+            className={`flex min-h-full w-full flex-col items-center justify-between min-[1024px]:h-full min-[1024px]:flex-row min-[1024px]:gap-8 min-[1024px]:py-0`}
         >
-            <Bio />
-            <Projects />
+            <div className='w-full'>
+                <Bio />
+            </div>
+            <div className='w-full'>
+                <ActiveProjects />
+            </div>
         </div>
     );
 }
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils';
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div
-            className={cn('animate-pulse rounded-md bg-primary/10 transition-all duration-300 ease-in-out', className)}
+            className={cn('animate-pulse rounded-md bg-primary/10 transition-all duration-1000 ease-in-out', className)}
             {...props}
         />
     );
