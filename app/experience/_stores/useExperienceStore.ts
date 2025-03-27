b9696a2e9ab1118fc80784be63f6610caab2281b
Matija Kočevar <matija.kocev@gmail.com>
2025-03-27 11:03:44 +0100

    Add real data for experience

--- app/experience/_stores/useExperienceStore.ts
+++ app/experience/_stores/useExperienceStore.ts
@@ -18,76 +18,51 @@ interface ExperienceStore {
 export const useExperienceStore = create<ExperienceStore>(() => ({
     experiences: [
         {
-            name: 'Junior Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2021, 0)],
-            description: 'I am a junior software engineer',
-            technologies: [{ name: 'React' }, { name: 'TypeScript' }, { name: 'TailwindCSS' }, { name: 'Node.js' }],
+            name: 'Frontend developer - Amibit d.o.o.',
+            dateRange: [new Date(2024, 0), new Date()],
+            description:
+                'Started a new job as a Vue.js developer. Working on both legacy Vue 2 applications and new React projects. Lead frontend developer for new React project.',
+            technologies: [{ name: 'Vue.js' }, { name: 'React' }, { name: 'Vite' }, { name: 'TypeScript' }],
         },
         {
-            name: 'Mid-Level Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2022, 0)],
-            description: 'I am a mid-level software engineer',
-            technologies: [{ name: 'Vue' }, { name: 'Javascript' }, { name: 'Boostrap' }, { name: 'C#' }],
+            name: 'Software engineer II - Frodx d.o.o.',
+            dateRange: [new Date(2023, 0), new Date(2024, 0)],
+            description:
+                'Developed React table library with advanced features. Worked on porting Laravel API to .NET. Created Microsoft Power Platform components.',
+            technologies: [{ name: 'React' }, { name: 'TypeScript' }, { name: '.NET' }, { name: 'PCF' }],
         },
         {
-            name: 'Senior Software Engineer',
-            dateRange: [new Date(2022, 0), new Date(2023, 0)],
-            description: 'I am a senior software engineer',
-            technologies: [{ name: 'Angular' }, { name: 'Javascript' }, { name: 'Materialize' }, { name: 'Java' }],
+            name: 'Software engineer - Dhimahi d.o.o.',
+            dateRange: [new Date(2023, 0), new Date(2023, 11)],
+            description:
+                'Worked on a large-scale Vue.js/Angular application with 24/7 availability. Gained experience with Linux and Docker.',
+            technologies: [{ name: 'Vue.js' }, { name: 'TypeScript' }, { name: 'Docker' }, { name: 'Linux' }],
         },
         {
-            name: 'Junior Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2021, 0)],
-            description: 'I am a junior software engineer',
-            technologies: [{ name: 'React' }, { name: 'TypeScript' }, { name: 'TailwindCSS' }, { name: 'Node.js' }],
+            name: 'Software developer - Agitavit Solutions d.o.o.',
+            dateRange: [new Date(2021, 0), new Date(2023, 0)],
+            description:
+                'Full-stack development with focus on React and TypeScript. Worked with .NET and SQL on backend.',
+            technologies: [{ name: 'React' }, { name: 'TypeScript' }, { name: '.NET' }, { name: 'SQL' }],
         },
         {
-            name: 'Mid-Level Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2022, 0)],
-            description: 'I am a mid-level software engineer',
-            technologies: [{ name: 'Vue' }, { name: 'Javascript' }, { name: 'Boostrap' }, { name: 'C#' }],
+            name: 'Learning web development',
+            dateRange: [new Date(2021, 0), new Date(2021, 11)],
+            description:
+                "After years of working in sales and having jobs where I didn't really enjoy myself, I began learning how to develop web apps. I used tutorials and independent searches to find out about modern frameworks and started making my own projects. The stacks I looked at were HTML, CSS, SQL, React JS, Vue JS, Node, .NET Core, and C#.",
+            technologies: [{ name: 'React' }, { name: 'Vue.js' }, { name: '.NET Core' }, { name: 'Bootstrap' }],
         },
         {
-            name: 'Senior Software Engineer',
-            dateRange: [new Date(2022, 0), new Date(2023, 0)],
-            description: 'I am a senior software engineer',
-            technologies: [{ name: 'Angular' }, { name: 'Javascript' }, { name: 'Materialize' }, { name: 'Java' }],
-        },
-        {
-            name: 'Junior Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2021, 0)],
-            description: 'I am a junior software engineer',
-            technologies: [{ name: 'React' }, { name: 'TypeScript' }, { name: 'TailwindCSS' }, { name: 'Node.js' }],
-        },
-        {
-            name: 'Mid-Level Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2022, 0)],
-            description: 'I am a mid-level software engineer',
-            technologies: [{ name: 'Vue' }, { name: 'Javascript' }, { name: 'Boostrap' }, { name: 'C#' }],
-        },
-        {
-            name: 'Senior Software Engineer',
-            dateRange: [new Date(2022, 0), new Date(2023, 0)],
-            description: 'I am a senior software engineer',
-            technologies: [{ name: 'Angular' }, { name: 'Javascript' }, { name: 'Materialize' }, { name: 'Java' }],
-        },
-        {
-            name: 'Junior Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2021, 0)],
-            description: 'I am a junior software engineer',
-            technologies: [{ name: 'React' }, { name: 'TypeScript' }, { name: 'TailwindCSS' }, { name: 'Node.js' }],
-        },
-        {
-            name: 'Mid-Level Software Engineer',
-            dateRange: [new Date(2021, 0), new Date(2022, 0)],
-            description: 'I am a mid-level software engineer',
-            technologies: [{ name: 'Vue' }, { name: 'Javascript' }, { name: 'Boostrap' }, { name: 'C#' }],
-        },
-        {
-            name: 'Senior Software Engineer',
-            dateRange: [new Date(2022, 0), new Date(2023, 0)],
-            description: 'I am a senior software engineer',
-            technologies: [{ name: 'Angular' }, { name: 'Javascript' }, { name: 'Materialize' }, { name: 'Java' }],
+            name: 'Data Analyst - Mimovrste d.o.o.',
+            dateRange: [new Date(2019, 0), new Date(2020, 11)],
+            description:
+                'Created semi-automated Excel documents for sales & stock overview. Developed automated price checking system using Google App Scripts. Created a web scraper with React.js for price comparison.',
+            technologies: [
+                { name: 'Excel' },
+                { name: 'Google Sheets' },
+                { name: 'Google App Scripts' },
+                { name: 'VBA' },
+            ],
         },
     ],
 }));
