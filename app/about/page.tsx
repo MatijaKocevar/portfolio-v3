52b60b93a15fb7632f6a36842d1802faa0c38b79
Matija Kočevar <matija.kocev@gmail.com>
2025-03-29 06:47:37 +0100

    Add loading component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,24 +1,10 @@
 import { useTranslations } from 'next-intl';
-import { getTranslations } from 'next-intl/server';
-import { MetaLocaleParams } from '../../types/locale';
 
-export async function generateMetadata({ params }: MetaLocaleParams) {
-    const { locale } = await params;
-    const t = await getTranslations({ locale });
-
-    const title = t('about.meta.title');
-    const description = t('about.meta.description');
-
-    return {
-        title,
-        description,
-    };
-}
-
-export default function About() {
+export default function AboutPage() {
     const t = useTranslations();
+
     return (
-        <div className='flex flex-col gap-4 p-8'>
+        <div className='flex flex-col gap-4'>
             <h1 className='mb-6 text-3xl font-bold'>{t('about.title')}</h1>
             <div>
                 <p>{t('about.description')}.</p>
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,21 +1,6 @@
 import { useTranslations } from 'next-intl';
-import { getTranslations } from 'next-intl/server';
-import { MetaLocaleParams } from '../../types/locale';
 
-export async function generateMetadata({ params }: MetaLocaleParams) {
-    const { locale } = await params;
-    const t = await getTranslations({ locale });
-
-    const title = t('skills.meta.title');
-    const description = t('skills.meta.description');
-
-    return {
-        title,
-        description,
-    };
-}
-
-export default function Skills() {
+export default function SkillsPage() {
     const t = useTranslations();
 
     return (
