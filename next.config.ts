31bb975528379ed9ba0b493960c49fabe2982e85
Matija Kočevar <matija.kocev@gmail.com>
2025-03-29 21:42:13 +0100

    More svgr fixes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
     images: {
         unoptimized: true,
         remotePatterns: [
