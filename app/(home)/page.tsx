305b9f781e3b4aedb28d1c09b300a22ecaa929dd
Matija Kočevar <matija.kocev@gmail.com>
2025-04-02 05:53:33 +0200

    Fix duplicate classes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ import ActiveProjects from './_components/ActiveProjects';
 export default function Home() {
     return (
         <div
-            className={`flex min-h-full w-full flex-col items-center justify-between lg:gap-8 min-[1024px]:h-full min-[1024px]:flex-row min-[1024px]:gap-4 min-[1024px]:py-0`}
+            className={`flex min-h-full w-full flex-col items-center justify-between min-[1024px]:h-full min-[1024px]:flex-row min-[1024px]:gap-8 min-[1024px]:py-0`}
         >
             <div className='w-full overflow-x-hidden'>
                 <Bio />
