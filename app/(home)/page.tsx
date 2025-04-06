60e524a253f8cf9f6cb0fb4e23bb39a7ee65cb5c
Matija Kočevar <matija.kocev@gmail.com>
2025-04-06 04:50:40 +0200

    Add loading spinners to pages

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Bio from './_components/Bio';
 import ActiveProjects from './_components/ActiveProjects';
 
-export default function Home() {
+export default async function Home() {
     return (
         <div className='flex h-full w-full flex-col overflow-x-hidden lg:items-center lg:justify-center'>
             <div className='flex w-full flex-col lg:flex-row lg:items-start lg:justify-evenly'>
