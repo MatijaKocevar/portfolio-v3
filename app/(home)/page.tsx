6b57d7c9863f9907ad685eebbd8fe5f0baf1a74a
Matija Kočevar <matija.kocev@gmail.com>
2025-07-18 04:34:01 +0200

    Fix home and experience layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,11 +3,9 @@ import ActiveProjects from './_components/active-projects';
 
 export default async function Home() {
     return (
-        <div className='flex h-full w-full flex-col overflow-x-hidden lg:items-center lg:justify-center'>
-            <div className='scrollable-element flex min-h-[calc(100vh-64px)] w-full flex-col lg:flex-row lg:items-start lg:justify-evenly'>
-                <Bio />
-                <ActiveProjects />
-            </div>
+        <div className='flex w-full flex-col overflow-x-hidden'>
+            <Bio />
+            <ActiveProjects />
         </div>
     );
 }
